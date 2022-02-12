a2879f6fc61e101083d48e37dba5e5d9cfbc3a74
Brennan Colberg <brennan.colberg@gmail.com>
2022-02-12 13:13:29 -0800

    show accurate player information

--- pages/[gameId].tsx
+++ pages/[gameId].tsx
@@ -1,51 +1,63 @@
-import { CellState, Game, GameState, Player } from "@prisma/client"
+import { CellState, Game, GameState, Player, PlayerState } from "@prisma/client"
 import { GetServerSideProps } from "next"
 import Board from "../components/Board"
 import prisma from "../prisma/client"
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 	const gameId = context.query.gameId as string
-	const gameState = await prisma.gameState.findFirst({
-		where: { gameId },
-		orderBy: { turn: "desc" },
-		include: {
-			game: { include: { players: true } },
-			cellStates: { orderBy: { index: "desc" }, include: { player: { select: { color: true } } } },
-		},
-	})
+	const userId = context.query.userId as string
+	const [gameState, player] = await Promise.all([
+		prisma.gameState.findFirst({
+			where: { gameId },
+			orderBy: { turn: "desc" },
+			include: {
+				game: true,
+				cellStates: {
+					orderBy: { index: "desc" },
+					include: { player: { select: { color: true } } },
+				},
+			},
+		}),
+		userId &&
+			prisma.player.findUnique({
+				where: { gameId_userId: { gameId, userId } },
+				include: { playerStates: { orderBy: { gameStateTurn: "desc" } } },
+			}),
+	])
 	return {
 		props: {
 			gameState,
 			game: gameState.game,
-			players: gameState.game.players,
+			player,
 		},
 	}
 }
 
 export default function IndexPage({
 	gameState,
+	player,
 	game,
 }: {
 	gameState: GameState & { cellStates: (CellState & { player?: Pick<Player, "color"> })[] }
+	player?: Player & { playerStates: PlayerState[] }
 	game: Game
 }): JSX.Element {
 	return (
 		<main>
-			<div>
-				<li>
-					turn: <span id="turn">2</span>
-				</li>
-				<li>
-					your color: <span id="color">blue</span>
-				</li>
-				<li>
-					upcoming steps: <span id="steps">1</span>
-				</li>
-				<li>
-					remaining moves: <span id="moves">8</span>
-				</li>
-				<button>Submit Moves</button>
-			</div>
+			{player && (
+				<div>
+					<li>
+						turn: <span id="turn">{gameState.turn + 1}</span>
+					</li>
+					<li>
+						your color: <span id="color">{player.color}</span>
+					</li>
+					<li>
+						remaining moves: <span id="moves">{player.playerStates[0].moves}</span>
+					</li>
+					<button>Submit Moves</button>
+				</div>
+			)}
 
 			<Board size={game.size} cellStates={gameState.cellStates} />
 		</main>
