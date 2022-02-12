0a6fe6a3f301d76d446b96fe5a1bd1ca93a088be
Brennan Colberg <brennan.colberg@gmail.com>
2022-02-12 09:21:27 -0800

    basic board

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,3 +1,11 @@
-export default function Board(): JSX.Element {
-	return <div className="board"></div>
+import { CSSProperties } from "react"
+
+export default function Board({ size }: { size: number }): JSX.Element {
+	return (
+		<div className="board" style={{ "--size": size } as CSSProperties}>
+			{[...new Array(size * size)].map((i) => (
+				<div key={i} />
+			))}
+		</div>
+	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,25 +4,24 @@ export default function IndexPage(): JSX.Element {
 	return (
 		<main>
 			<div>
-				<h1>
-					Turn: <span id="turn">2</span>
-				</h1>
-				<h2>
-					you are <span id="color">blue</span>
-				</h2>
+				<li>
+					turn: <span id="turn">2</span>
+				</li>
+				<li>
+					your color: <span id="color">blue</span>
+				</li>
+				<li>
+					upcoming steps: <span id="steps">1</span>
+				</li>
+				<li>
+					remaining moves: <span id="moves">8</span>
+				</li>
+				<button>Submit Moves</button>
 			</div>
 
-			<Board />
+			<Board size={20} />
 
-			<div>
-				<h1>
-					Upcoming Steps: <span id="steps">1</span>
-				</h1>
-				<h2>
-					Moves: <span id="moves">8</span>
-				</h2>
-				<button>Submit</button>
-			</div>
+			<div></div>
 		</main>
 	)
 }
