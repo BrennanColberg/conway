8ee107379446e24b60562482e7adc53be8056b4b
Brennan Colberg <brennan.colberg@gmail.com>
2022-02-12 09:04:35 -0800

    basic html

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
+import Board from "../components/Board"
+
 export default function IndexPage(): JSX.Element {
-	return <h1>hello world</h1>
+	return (
+		<main>
+			<div>
+				<h1>
+					Turn: <span id="turn">2</span>
+				</h1>
+				<h2>
+					you are <span id="color">blue</span>
+				</h2>
+			</div>
+
+			<Board />
+
+			<div>
+				<h1>
+					Upcoming Steps: <span id="steps">1</span>
+				</h1>
+				<h2>
+					Moves: <span id="moves">8</span>
+				</h2>
+				<button>Submit</button>
+			</div>
+		</main>
+	)
 }
