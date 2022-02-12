0404dcf841e897eb8f79e88aa58884159df9c823
Brennan Colberg <brennan.colberg@gmail.com>
2022-02-12 10:15:10 -0800

    allow for createion of users with non uuid ids

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -4,7 +4,12 @@ import prisma from "../../prisma/client"
 const handler: NextApiHandler = async (req, res) => {
 	if (req.method !== "POST") return res.status(405).send("invalid method")
 
-	const user = await prisma.user.create({ data: {} })
+	const id = req.body.id as string | undefined
+
+	const user =
+		id !== undefined && id.length <= 36
+			? await prisma.user.upsert({ where: { id }, create: { id }, update: {} })
+			: await prisma.user.create({ data: {} })
 
 	res.status(200).json(user)
 }
