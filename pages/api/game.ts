0ddd5db6031b1c22ff63127b3e590a418d3264fb
Brennan Colberg <brennan.colberg@gmail.com>
2022-02-12 18:36:52 -0800

    integrate initial game state into game creation

--- pages/api/game.ts
+++ pages/api/game.ts
@@ -14,9 +14,35 @@ const handler: NextApiHandler = async (req, res) => {
 			size,
 			players: { createMany: { data: players } },
 		},
+		include: { players: true },
 	})
 
-	res.status(200).json(game)
+	const cellStates = [...Array(game.size * game.size)].map((_, index) => {
+		const alive = Math.random() > 0.5
+		if (!alive) return { index, alive: false }
+		const playerIndex = Math.floor(Math.random() * game.players.length)
+		const player = game.players[playerIndex]
+		return { index, alive: true, playerUserId: player.userId, playerGameId: player.gameId }
+	})
+
+	const gameState = await prisma.gameState.create({
+		data: {
+			game: { connect: { id: game.id } },
+			turn: 0,
+			cellStates: { createMany: { data: cellStates } },
+			playerStates: {
+				createMany: {
+					data: game.players.map((player) => ({
+						playerGameId: player.gameId,
+						playerUserId: player.userId,
+						moves: 10,
+					})),
+				},
+			},
+		},
+	})
+
+	res.status(200).json({ game, gameState })
 }
 
 export default handler
