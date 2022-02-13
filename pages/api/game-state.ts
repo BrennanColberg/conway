ae958743c5bf1f9280f4a3cb4b84d6567d4c0677
Brennan Colberg <brennan.colberg@gmail.com>
2022-02-13 13:53:38 -0800

    /api/game-state to new schema

--- pages/api/game-state.ts
+++ pages/api/game-state.ts
@@ -1,4 +1,3 @@
-import { CellState } from "@prisma/client"
 import { NextApiHandler } from "next"
 import prisma from "../../prisma/client"
 
@@ -9,78 +8,54 @@ const handler: NextApiHandler = async (req, res) => {
 
 	const lastState = await prisma.gameState.findFirst({
 		where: { gameId },
-		include: {
-			game: true,
-			cellStates: {
-				orderBy: { index: "asc" },
-				select: { playerGameId: true, playerUserId: true, alive: true, index: true },
-			},
-			playerStates: { select: { moves: true, playerGameId: true, playerUserId: true } },
-		},
+		include: { game: true },
 		orderBy: { turn: "desc" },
 	})
 	if (!lastState) return res.status(404).send("game state not found")
 
 	const size = lastState.game.size
-	const cellStates: Omit<CellState, "gameStateTurn" | "gameStateGameId">[] =
-		lastState.cellStates.map((cellState) => {
-			const i = cellState.index
-			const x = i % size
-			const y = Math.floor(i / size)
-			const onTop = y === 0
-			const onBottom = y === size - 1
-			const onLeft = x === 0
-			const onRight = x === size - 1
-			const neighbors = [
-				onTop || onLeft ? undefined : i - size - 1,
-				onTop ? undefined : i - size,
-				onTop || onRight ? undefined : i - size + 1,
-				onLeft ? undefined : i - 1,
-				onRight ? undefined : i + 1,
-				onBottom || onLeft ? undefined : i + size - 1,
-				onBottom ? undefined : i + size,
-				onBottom || onRight ? undefined : i + size + 1,
-			]
-				.filter((ni) => ni !== undefined)
-				.map((ni) => lastState.cellStates[ni])
-			const aliveNeighbors = neighbors.filter((neighbor) => neighbor.alive)
+	const cells = lastState.cells.map((cell, i) => {
+		const x = i % size
+		const y = Math.floor(i / size)
+		const onTop = y === 0
+		const onBottom = y === size - 1
+		const onLeft = x === 0
+		const onRight = x === size - 1
+		const neighbors = [
+			onTop || onLeft ? undefined : i - size - 1,
+			onTop ? undefined : i - size,
+			onTop || onRight ? undefined : i - size + 1,
+			onLeft ? undefined : i - 1,
+			onRight ? undefined : i + 1,
+			onBottom || onLeft ? undefined : i + size - 1,
+			onBottom ? undefined : i + size,
+			onBottom || onRight ? undefined : i + size + 1,
+		]
+			.filter((ni) => ni !== undefined)
+			.map((ni) => lastState.cells[ni])
+		const aliveNeighbors = neighbors.filter((neighbor) => neighbor !== -1)
 
-			if (cellState.alive) {
-				if (aliveNeighbors.length <= 1 || aliveNeighbors.length >= 4) {
-					// die
-					return {
-						index: cellState.index,
-						alive: false,
-						playerGameId: null,
-						playerUserId: null,
-					}
-				} else return cellState
-			} else {
-				if (aliveNeighbors.length === 3) {
-					// come to life
-					const userIds = aliveNeighbors.map((neighbor) => neighbor.playerUserId)
-					const userId =
-						userIds[0] === userIds[1] || userIds[0] === userIds[2]
-							? userIds[0]
-							: userIds[1] === userIds[2]
-							? userIds[1]
-							: userIds[Math.floor(Math.random() * 3)]
-					return {
-						playerUserId: userId,
-						playerGameId: lastState.gameId,
-						alive: true,
-						index: cellState.index,
-					}
-				} else return cellState
-			}
-		})
+		if (cell !== -1) {
+			if (aliveNeighbors.length <= 1 || aliveNeighbors.length >= 4) return -1
+			// die
+			else return cell
+		} else {
+			if (aliveNeighbors.length === 3)
+				// come to life
+				return aliveNeighbors[0] === aliveNeighbors[1] || aliveNeighbors[0] === aliveNeighbors[2]
+					? aliveNeighbors[0]
+					: aliveNeighbors[1] === aliveNeighbors[2]
+					? aliveNeighbors[1]
+					: aliveNeighbors[Math.floor(Math.random() * 3)]
+			else return cell
+		}
+	})
 
 	const newState = await prisma.gameState.create({
 		data: {
 			turn: lastState.turn + 1,
 			game: { connect: { id: lastState.gameId } },
-			playerStates: { createMany: { data: lastState.playerStates } },
-			cellStates: { createMany: { data: cellStates } },
+			cells,
 		},
 	})
 
