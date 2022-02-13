7f9de35e22994cd4a1ea2beac136ac7b55c75544
Brennan Colberg <brennan.colberg@gmail.com>
2022-02-13 13:42:41 -0800

    /api/game to new schema

--- pages/api/game.ts
+++ pages/api/game.ts
@@ -1,44 +1,36 @@
-import { Player } from "@prisma/client"
 import { NextApiHandler } from "next"
 import prisma from "../../prisma/client"
 
 const handler: NextApiHandler = async (req, res) => {
 	if (req.method !== "POST") return res.status(405).send("wrong method")
 	const id = req.body.id as string | undefined
-	const size = (req.body.size as number) || 10
-	const players = (req.body.players as Pick<Player, "userId" | "color">[]) || []
+	let size = req.body.size as number | undefined
+	if (size === undefined || size < 4) size = 8
+	let playerCount = req.body.playerCount as number | undefined
+	if (playerCount === undefined || playerCount < 2) playerCount = 2
 
 	const game = await prisma.game.create({
 		data: {
 			id,
 			size,
-			players: { createMany: { data: players } },
+			playerCount,
 		},
-		include: { players: true },
 	})
 
-	const cellStates = [...Array(game.size * game.size)].map((_, index) => {
-		const alive = Math.random() > 0.5
-		if (!alive) return { index, alive: false }
-		const playerIndex = Math.floor(Math.random() * game.players.length)
-		const player = game.players[playerIndex]
-		return { index, alive: true, playerUserId: player.userId, playerGameId: player.gameId }
+	// generate an initial configuration of cells for the full board
+	const cells = [...Array(game.size * game.size)].map(() => {
+		// check if the cell is empty
+		if (Math.random() > 0.5) return -1
+		// if not empty, assign to a random player
+		return Math.floor(Math.random() * playerCount)
 	})
 
 	const gameState = await prisma.gameState.create({
 		data: {
 			game: { connect: { id: game.id } },
 			turn: 0,
-			cellStates: { createMany: { data: cellStates } },
-			playerStates: {
-				createMany: {
-					data: game.players.map((player) => ({
-						playerGameId: player.gameId,
-						playerUserId: player.userId,
-						moves: 10,
-					})),
-				},
-			},
+			cells,
+			moves: [...Array(game.playerCount)].map(() => 5),
 		},
 	})
 
