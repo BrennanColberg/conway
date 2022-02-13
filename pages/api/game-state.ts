b3108d41e488cd9cbc78602982fba84837249f40
Brennan Colberg <brennan.colberg@gmail.com>
2022-02-13 14:13:47 -0800

    GET /api/game-state

--- pages/api/game-state.ts
+++ pages/api/game-state.ts
@@ -1,7 +1,15 @@
 import { NextApiHandler } from "next"
+import getCurrentGameState from "../../lib/getCurrentGameState"
 import prisma from "../../prisma/client"
 
 const handler: NextApiHandler = async (req, res) => {
+	if (req.method === "GET") {
+		const gameId = req.query.gameId as string
+		const turn = req.query.turn && parseInt(req.query.turn as string)
+		const result = await getCurrentGameState(gameId, turn)
+		return res.status(200).json(result)
+	}
+
 	if (req.method !== "POST") return res.status(405).send("invalid method")
 	const gameId = req.body.gameId as string
 	if (gameId === undefined) return res.status(400).send("no gameId")
